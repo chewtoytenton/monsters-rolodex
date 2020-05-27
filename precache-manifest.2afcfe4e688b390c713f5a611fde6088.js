34c76d7313d861c563d50bd7c042a34f3e4cd6d2
Chewtoy <chewtoy@pop-os.localdomain>
2020-05-28 00:23:12 +0300

    Updates

---
+++ precache-manifest.2afcfe4e688b390c713f5a611fde6088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938ded8b7272bfa503627a539ce6e6eb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4069c38fee40e5415906",
+    "url": "/monsters-rolodex/static/css/main.d494e881.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4069c38fee40e5415906",
+    "url": "/monsters-rolodex/static/js/main.bb8c7b48.chunk.js"
+  },
+  {
+    "revision": "df5d6ef2e44460cbab72",
+    "url": "/monsters-rolodex/static/js/runtime-main.51942013.js"
+  }
+]);
